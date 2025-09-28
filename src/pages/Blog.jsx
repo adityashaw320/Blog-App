c10bd1383c6e8b5db09ae1e7c7276b9775257b8d
adityashaw320 <silentadhi1234@gmail.com>
2025-09-28 11:32:34 +0530

    Add footer and newsletter components, update Blog page
    Introduced new Foooter and NewsLetter components and integrated them into the Home page. Updated the Blog page to fetch and display blog data based on route parameters, showing a loading state while fetching. These changes enhance the site's layout and user engagement features.

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
+
 
 const Blog = () => {
-  return (
+
+  const {id} = useParams()
+
+  const [data, setData] = useState(null)
+
+  const fetchBlogData = async()=>{
+   const data = blog_data.find(item => item._id === id)
+   setData(data)
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  },[])
+
+  return data ? (
     <div>
-      <h1>Blog</h1>
+      
+      <Navbar/>
+
+      <div></div>
+
+
     </div>
-  )
+  ) : <div>Loading....</div>
 }
 
 export default Blog
